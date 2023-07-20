1b8b3d545073183a6eb7d65219f33c7488652f6a
Igor Ribeiro <igor.piresmeira@gmail.com>
2023-07-20 01:22:05 -0300

    Desktop screen e mais efeitos

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 $(document).ready(function () {
   $("#home").fadeIn("fast");
+  $(".home-button").addClass("selected");
 
   $(".home-button").on("click", function () {
+    $(".home-button").addClass("selected");
+    $(".modeon-button").removeClass("selected");
+
     $("#mode-on").fadeOut(1000);
     $("#mode-off").fadeOut(1000);
     $("#home").fadeIn(1000);
   });
   $(".modeon-button").on("click", function () {
+    $(".modeon-button").addClass("selected");
+    $(".home-button").removeClass("selected");
+    $(".modeoff-button").removeClass("selected");
+
     $("#home").fadeOut(1000);
     $("#mode-off").fadeOut(1000);
     $("#mode-on").fadeIn(1000);
   });
   $(".modeoff-button").on("click", function () {
+    $(".modeoff-button").addClass("selected");
+    $(".home-button").removeClass("selected");
+    $(".modeon-button").removeClass("selected");
+
     $("#home").fadeOut(1000);
     $("#mode-on").fadeOut(1000);
     $("#mode-off").fadeIn(1000);
+  });
+
+  $("a[href='#search']").on("click", function () {
+    $(".search-form").toggleClass("active");
+  });
+
+  $("section").on("click", function () {
+    $(".search-form").removeClass("active");
   });
 });
